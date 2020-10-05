6689aff8f79368d81a16bdb9e97d0c707c52ad55
NguyenHungHust256 <hungpronguyen256@gmail.com>
2020-10-05 11:12:40 +0700

    manage-employee-angular

---
+++ src/app/employee-details/employee-details.component.ts
@@ -0,0 +1,33 @@
+import { EmployeeService } from './../employee.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Employee } from '../employee';
+import { error } from 'protractor';
+@Component({
+  selector: 'app-employee-details',
+  templateUrl: './employee-details.component.html',
+  styleUrls: ['./employee-details.component.css']
+})
+export class EmployeeDetailsComponent implements OnInit {
+
+  id: number;
+  employee: Employee;
+  constructor(private route: ActivatedRoute, private router: Router,
+    private employeeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.employee = new Employee();
+    this.id = this.route.snapshot.params['id'];
+
+    this.employeeeService.getEmployee(this.id)
+      .subscribe(data => {
+        console.log(data);
+        this.employee = data;
+      }, error => console.log(error));
+  }
+
+  list(){
+    this.router.navigate(['employees']);
+  }
+
+}
